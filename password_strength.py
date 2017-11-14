b8d197ababb11526fa07da9817e60aa2cae2e507
Kirill Maslov <maslovks@gmail.com>
2017-11-15 01:45:30 +0300

    правки

--- password_strength.py
+++ password_strength.py
@@ -5,91 +5,83 @@ from string import ascii_uppercase
 from string import punctuation
 from string import digits
 
+PATHS_OF_UNDESIRED_WORDS_TO_USE = {'names': './blacklist/names.txt',
+                                   'surnames': './blacklist/surnames.txt',
+                                   'english words': './blacklist/words.txt'}
 
-def is_in_blacklist(password):
+def is_in_stop_list(password):
 
-    path_file = './blacklist/popular10000pass.txt'
+    path_of_stop_list = './blacklist/popular10000pass.txt'
     try:
-        with open(path_file) as black_list:
+        with open(path_of_stop_list) as black_list:
             for word in black_list:
                 if password == word.rstrip():
                     return True
             return False
     except FileNotFoundError:
-        print('File not found path:' + path_file)
+        print('File not found path: {}'.format(path_of_stop_list))
 
 
-def contains_words(password, words_list_path):
+def contains_undesired_words(password, path_of_undesired_words_list):
 
     password = password.lower()
     try:
-        with open(words_list_path) as black_list:
-            for word in black_list:
+        with open(path_of_undesired_words_list) as stop_list:
+            for word in stop_list:
                 word = word.rstrip().lower()
-                min_length_of_word = 3
-                if len(word) > min_length_of_word and word in password:
+                min_reasonable_length_of_word = 3
+                if len(word) > min_reasonable_length_of_word and \
+                                word in password:
                     return True
             return False
     except FileNotFoundError:
-        print('File not found path:' + words_list_path)
+        print('File not found path: {}'.format(path_of_undesired_words_list))
 
 
-def variety_of_symbols(password, print_details=False):
+def proportion_of_symbol_types_used(password):
 
-    types_of_symbols = {'uppercase': ascii_uppercase,
-                        'lowercase': ascii_lowercase,
-                        'digits': digits,
-                        'punctuation': punctuation}
-    types_found = 0
-    for keys, type_list in types_of_symbols.items():
+    types_of_symbols_map = {'uppercase': ascii_uppercase,
+                            'lowercase': ascii_lowercase,
+                            'digits': digits,
+                            'punctuation': punctuation}
+    types_of_symbols_found = 0
+    for symbol_types, type_list in types_of_symbols_map.items():
         for char in password:
             if char in type_list:
-                types_found += 1
+                types_of_symbols_found += 1
                 break
-        else:
-            if print_details:
-                print('-25% PENALTY: ' + keys + ' symbols not found!!!')
 
-    return types_found / len(types_of_symbols)
+    return types_of_symbols_found / len(types_of_symbols_map)
 
 
-def get_password_strength(password, print_details=False):
+def get_password_strength(password):
 
-    word_list_path = {'names': './blacklist/names.txt',
-                      'surnames': './blacklist/surnames.txt',
-                      'english words': './blacklist/words.txt'}
+    min_possible_strength_score = 1
+    max_possible_strength_score = 10
 
-    if is_in_blacklist(password):
-        if print_details:
-            print('Your password is in black list!')
-        return 1
+    if is_in_stop_list(password):
+        return min_possible_strength_score
+
     starting_score = 10
+    strength_score = starting_score
 
-    recommended_length = 12
-    starting_score *= len(password) / recommended_length
+    recommended_password_length = 12
+    strength_score *= len(password) / recommended_password_length
 
-    starting_score *= variety_of_symbols(password, print_details)
+    strength_score *= proportion_of_symbol_types_used(password)
 
-    if print_details and len(password) < recommended_length:
-        print('{:.0f}% PENALTY: minimum 12 charecters length recommended.'
-              .format((len(password)/recommended_length - 1)*100), )
-
-    for key, word_path in word_list_path.items():
-        if contains_words(password, word_path):
+    for path_of_undesired_word_list in PATHS_OF_UNDESIRED_WORDS_TO_USE.values():
+        if contains_undesired_words(password, path_of_undesired_word_list):
             penalty_multiplier = 0.75
-            starting_score *= penalty_multiplier
-            if print_details:
-                print('-25% PENALTY: password shouldn\'t contain ' + key)
+            strength_score *= penalty_multiplier
 
-    min_score = 1
-    max_score = 10
-    starting_score = max(starting_score, min_score)
-    starting_score = min(starting_score, max_score)
+    strength_score = max(strength_score, min_possible_strength_score)
+    strength_score = min(strength_score, max_possible_strength_score)
 
-    return starting_score
+    return strength_score
 
 
 if __name__ == '__main__':
-    password = getpass.getpass(prompt="Type the password to evaluate:")
+    password = getpass.getpass(prompt="Type the password to evaluate: ")
     print("The strength of the password: {:.1f}"
-          .format(get_password_strength(password, True)))
+          .format(get_password_strength(password)))
--- tests.py
+++ tests.py
@@ -1,51 +1,51 @@
 import unittest
 
-from password_strength import is_in_blacklist
-from password_strength import contains_words
-from password_strength import variety_of_symbols
+from password_strength import PATHS_OF_UNDESIRED_WORDS_TO_USE
+from password_strength import is_in_stop_list
+from password_strength import contains_undesired_words
+from password_strength import proportion_of_symbol_types_used
 from password_strength import get_password_strength
 
 
 class PasswordStrengthTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.words_list_dict = {'names': './blacklist/names.txt',
-                                'surnames': './blacklist/surnames.txt',
-                                'words': './blacklist/words.txt'}
+    def test_is_in_stop_list(self):
+        self.assertTrue(is_in_stop_list('12345'))
+        self.assertFalse(is_in_stop_list('afFgsS123131'))
 
-    def test_is_in_blacklist(self):
-        self.assertTrue(is_in_blacklist('12345'))
-        self.assertFalse(is_in_blacklist('afFgsS123131'))
-
-    def test_contains_words(self):
+    def test_contains_undesired_words(self):
 
         self.assertTrue(
-            contains_words('George3345', self.words_list_dict['names']))
+            contains_undesired_words('George3345',
+                                     PATHS_OF_UNDESIRED_WORDS_TO_USE['names']))
         self.assertFalse(
-            contains_words('!234asdsgd', self.words_list_dict['names']))
+            contains_undesired_words('!234asdsgd',
+                                     PATHS_OF_UNDESIRED_WORDS_TO_USE['names']))
 
         self.assertTrue(
-            contains_words('Bush1234', self.words_list_dict['surnames']))
+            contains_undesired_words('Bush1234',
+                                     PATHS_OF_UNDESIRED_WORDS_TO_USE['surnames']))
         self.assertFalse(
-            contains_words('adssf9g23', self.words_list_dict['surnames']))
+            contains_undesired_words('adssf9g23',
+                                     PATHS_OF_UNDESIRED_WORDS_TO_USE['surnames']))
 
         self.assertTrue(
-            contains_words('@apple123', self.words_list_dict['words']))
+            contains_undesired_words('@apple123',
+                                     PATHS_OF_UNDESIRED_WORDS_TO_USE['english words']))
         self.assertFalse(
-            contains_words('a!dgdD123', self.words_list_dict['words']))
+            contains_undesired_words('a!dgdD123',
+                                     PATHS_OF_UNDESIRED_WORDS_TO_USE['english words']))
 
-    def test_varienty_of_symbols(self):
-        self.assertEqual(variety_of_symbols('password'), 0.25)
-        self.assertEqual(variety_of_symbols('Password'), 0.5)
-        self.assertEqual(variety_of_symbols('Password123'), 0.75)
-        self.assertEqual(variety_of_symbols('Password123!@++'), 1)
+    def test_proportion_of_symbol_types_used(self):
+        self.assertEqual(proportion_of_symbol_types_used('password'), 0.25)
+        self.assertEqual(proportion_of_symbol_types_used('Password'), 0.5)
+        self.assertEqual(proportion_of_symbol_types_used('Password123'), 0.75)
+        self.assertEqual(proportion_of_symbol_types_used('Password123!@++'), 1)
 
     def test_get_password_strength(self):
         self.assertEqual(get_password_strength('1asdgjHHm9++!2dd'), 10)
         self.assertEqual(get_password_strength('12345'), 1)
-        self.assertEqual(get_password_strength('George12!!34241+'), 0.75)
-        self.assertEqual(get_password_strength('ElenaBielinski123!!!'), 0.5)
-        self.assertEqual(get_password_strength('Ak1!l5'), 0.5)
+
 
 if __name__ == '__main__':
     unittest.main
